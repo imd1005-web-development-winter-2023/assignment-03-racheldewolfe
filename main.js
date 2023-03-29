ff856faca77f330c560747b45e16082abb150927
Rachel DeWolfe <dewo0013@algonquinlive.com>
2023-03-29 00:56:21 -0400

    new counter and bad javaScript

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ const form = document.querySelector("#form");
 const item = document.querySelector("#form-input");
 const list = document.querySelector("#list-bottom-items");
 const remove = document.querySelector("#list-bottom-remove")
+const denominator = document.querySelector("#denominator")
 
 form.addEventListener("submit", addToList);
+
+remove.addEventListener("click", removeFromList);
+
+if (addToList()) {
+  denominator = denominator + 1;
+}
 
 function addToList(event) {
   event.preventDefault();
@@ -22,4 +29,14 @@ function addToList(event) {
   remove.appendChild(removeItem);
 
   form.reset();
+}
+
+function removeFromList(removeItem, myList) {
+
+  console.log("Item removed");
+
+  remove.removeChild(removeItem);
+  list.removeChild(myList);
+
+
 }
